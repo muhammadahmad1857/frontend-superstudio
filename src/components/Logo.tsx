dba4aeb8874d93c3f8dabefe0d25e0a8de42de01
= <ahmedjawad1857@gmail.com>
2026-01-09 17:29:22 +0500

    Removed react

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,6 +6,4 @@ const Logo = () => {
     <img className='size-full object-contain' src={isDark ? '/Apollo nft-01-white.png' : '/Apollo nft-01-black.png'}/>
   )
 }
-
-export default Logo
-;
+export default Logo;
